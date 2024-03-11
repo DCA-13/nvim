b983684f2731d4e0079afa1386f436b731c4f5b8
DCA-13 <davidcampos1312@gmail.com>
2024-03-11 18:59:46 +0100

    disable wilder (reenable when python fixed)

---
+++ lua/custom/plugins/wilder.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'gelguy/wilder.nvim',
+    enabled = false,
+    keys = {
+      ':',
+      -- '/',
+      -- '?',
+    },
+    config = function()
+      local wilder = require 'wilder'
+      wilder.setup { modes = { ':', '/', '?' } }
+      -- wilder.set_option('pipeline', {
+      --   wilder.branch(
+      --     wilder.cmdline_pipeline {
+      --       fuzzy = 1,
+      --     },
+      --     wilder.vim_search_pipeline {
+      --       fuzzy = 1,
+      --     }
+      --   ),
+      -- })
+    end,
+  },
+}
